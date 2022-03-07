0b83a5a0d91a268e9257a3bf4ec64176ba78463b
Valentin Manaila <valentin.manaila@gmail.com>
2022-03-07 00:02:58 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d59ee770a44be490b289.js"
+    "url": "webpack-runtime-abd7940619378514b988.js"
   },
   {
     "url": "framework-45f0bfe9f06e0e8da7d0.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "26e071a0ed0ab329e90dfb267304ad77"
+    "revision": "0330022c6d013ecb58985f06bf4889c6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "862505f9210220c0f0e40b7cfb05f0e4"
+    "revision": "5bc7d99cbb96f359e3f9a2bab03dffeb"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
